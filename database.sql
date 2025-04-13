1d461a1e902e79fe91c9b2b0780ef351c784e57f
김성훈 (SungHoon Kim) <155641852+hackkim@users.noreply.github.com>
2025-04-13 11:01:05 +0900

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,1 +1,11 @@
-CREATE TABLE users (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(100) NOT NULL UNIQUE, password VARCHAR(255) NOT NULL);
+Access MySQL by entering the following in the terminal: mysql -u root -p
+  
+Create the tools_db Database: CREATE DATABASE tools_db;
+
+Use the tools_db Database: USE tools_db;
+
+Create the users Table: CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
